1f6793301c30e939ebde123a05da8375006ea6f7
andrea-come <60099460+andrea-come@users.noreply.github.com>
2026-01-01 19:25:09 +0100

    Update README.d

--- Linge & Langtangen - Programming for Computations with MATLAB-Octave/README.d
+++ Linge & Langtangen - Programming for Computations with MATLAB-Octave/README.d
@@ -1,1 +1,1 @@
-Solutions of some exercices from the book: https://link.springer.com/book/10.1007/978-3-319-32452-4
+Solutions of some exercices from the book: [Linge & Langtangen - Programming for Computations with MATLAB/Octave](https://link.springer.com/book/10.1007/978-3-319-32452-4)

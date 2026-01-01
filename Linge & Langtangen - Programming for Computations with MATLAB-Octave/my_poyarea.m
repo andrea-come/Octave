53e2dd3b7125a473e9ab6fdb83268abb373dc7d8
andrea-come <60099460+andrea-come@users.noreply.github.com>
2026-01-01 19:13:55 +0100

    Create my_poyarea.m

---
+++ Linge & Langtangen - Programming for Computations with MATLAB-Octave/my_poyarea.m
@@ -0,0 +1,28 @@
+function res = my_polyarea(xs, ys)
+%MY_POLYAREA Computes the area of a 2D polygon given its vertices.
+%   RES = MY_POLYAREA(XS, YS) calculates the area of a planar polygon
+%   defined by the vertices with coordinates XS and YS.
+%
+%   The function implements the Gauss Area Formula (Shoelace Formula).
+%   The input vectors must satisfy the following conditions:
+%       1. They must have the same length.
+%       2. The vertices must be ordered consecutively (clockwise or 
+%          counter-clockwise).
+%
+%   Inputs:
+%       XS - Vector of x-coordinates (double).
+%       YS - Vector of y-coordinates (double).
+%
+%   Output:
+%       RES - The calculated area (positive scalar).
+  if nargin < 2
+    error("Two input arguments are required: xs and ys.");
+  endif
+  x_len = length(xs); y_len = length(ys);
+  if x_len ~= y_len
+    error("The input vectors must have same length")
+  endif
+  xs = xs(:); ys = ys(:);
+  n = x_len;
+  res = 0.5 * abs( sum(xs .* circshift(ys, -1) - ys .* circshift(xs, -1)));
+endfunction
